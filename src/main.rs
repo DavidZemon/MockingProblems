75b415d4c44ebde9a71965594722bc202602b816
David Zemon <david@zemon.name>
2023-04-20 16:06:24 -0500

    test: Add test that fails to compile

--- src/main.rs
+++ src/main.rs
@@ -36,3 +36,37 @@ fn main() {
     my_struct.do_foo();
     println!("The foo is done!");
 }
+
+#[cfg(test)]
+mod test {
+    use mockall::mock;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    use crate::{NotMyCodeTrait, ThisIsMyStruct};
+
+    mock! {
+        MyNotMyCodeTrait {}
+
+        impl NotMyCodeTrait for MyNotMyCodeTrait {
+            fn foo(&self);
+        }
+    }
+
+    struct TestContext {
+        mock_port: Rc<RefCell<Box<MockMyNotMyCodeTrait>>>,
+        testable: ThisIsMyStruct,
+    }
+
+    impl TestContext {
+        fn new() -> Self {
+            let mocked_dependency = Rc::new(RefCell::new(Box::new(MockMyNotMyCodeTrait::new())));
+            Self {
+                mock_port: Rc::clone(&mocked_dependency),
+                testable: ThisIsMyStruct {
+                    dependency: mocked_dependency,
+                },
+            }
+        }
+    }
+}
